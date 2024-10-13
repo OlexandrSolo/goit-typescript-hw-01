829c1a8f62db93d49c300ac78bbc33f527501ddd
Your Name <you@example.com>
2024-10-13 14:12:42 +0300

    task-2.2 - fix

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -1,15 +1,18 @@
-  type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number
-}
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
 
-function compare <T extends AllType>(top: T, bottom: T): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  }
+type top = Pick<AllType, "name" | "color">;
+type bottom = Pick<AllType, "position" | "weight">;
+
+function compare<T extends top, U extends bottom>(top: T, bottom: U): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
 }
