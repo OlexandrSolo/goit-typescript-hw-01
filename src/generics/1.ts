03182f9a06a100136dac526d04c29c21af0c75ee
Your Name <you@example.com>
2024-10-13 17:44:17 +0300

    task-2.2 - fix

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-async function fetchData(url:string) :Promise<object>{
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    throw new Error(`Error fetching from ${url}: ${error}`);
-  }
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
 }
